7d9baded48e599d2948ce97a1cd9eda5b5c4f178
Michael Mendoza <askmichaelmendoza@gmail.com>
2021-10-25 18:20:37 -0700

    Updated styles/UI

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,13 +3,13 @@ import { winValue } from "../libs/GameState";
 const Board = ({board}) => {
 
     const getClassName = (value) => {
-        return value === winValue ? 'cell win' : 'cell';
+        return `cell cell-${value}`;
     }
-
+    
     return (
         <div className='board'>
             { 
-                board.map((cell, index) => <div className={getClassName(cell)} key={index}> {cell} </div>)
+                board.map((cell, index) => <div className={getClassName(cell)} key={index}> {cell ? cell : ''} </div>)
             }
         </div>
     )
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -22,9 +22,10 @@ const Game = () => {
     return (
         <div>
             <header className='header'> 2048 </header>
-            <section className='viewport'> 
-                <div className='layout-row-center'>
-                    <button onClick={handleNewBoard}> New </button>
+            <section className='viewport layout-column-center'> 
+               
+                <div className='board-header'>
+                    <button onClick={handleNewBoard}> New Game </button>
                 </div>
 
                 <div className='layout-row-center'>
@@ -38,10 +39,35 @@ const Game = () => {
                 <div>
                     { gameState.isGameWon ? <div className='game-over-message'> Game Won!! </div>: null }
                 </div>
+
+                <div className='instructions'>
+                    <p>
+                        HOW TO PLAY: Use your arrow keys to move the tiles. Tiles with the same number merge into one when they touch. Add them up to reach 2048!
+                    </p>
+                </div>
             </section>
         </div>
     )
     
+}
+
+const ColorPallete = () => {
+    return (
+        <div> 
+            <span style={{padding:'1em'}} className='cell-0'> 2 </span>
+            <span style={{padding:'1em'}} className='cell-2'> 2 </span>
+            <span style={{padding:'1em'}} className='cell-4'> 4 </span>
+            <span style={{padding:'1em'}} className='cell-8'> 8 </span>
+            <span style={{padding:'1em'}} className='cell-16'> 16 </span>
+            <span style={{padding:'1em'}} className='cell-32'> 32 </span>
+            <span style={{padding:'1em'}} className='cell-64'> 64 </span>
+            <span style={{padding:'1em'}} className='cell-128'> 128 </span>
+            <span style={{padding:'1em'}} className='cell-256'> 256 </span>
+            <span style={{padding:'1em'}} className='cell-512'> 512 </span>
+            <span style={{padding:'1em'}} className='cell-1024'> 1024 </span>
+            <span style={{padding:'1em'}} className='cell-2048'> 2048 </span>
+        </div>
+    )
 }
 
 export default Game;
